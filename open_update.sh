7cac19f08052295ffa941c5cec55329936cf81a0
Mycadi <6507272@qq.com>
2019-01-14 17:07:56 +0800

    解决重复执行导致BUG问题，解决openssh7.9p1升级问题。

--- open_update.sh
+++ open_update.sh
@@ -11,6 +11,7 @@ Opensshv=`ls /root/openssl_up/openssh-*.tar.gz | awk -F/ '{print $4}' |awk -F.ta
 UJ=`awk '/processor/{i++}END{print i}' /proc/cpuinfo`
 Adir=/root/openssl_up
 PORT=$1
+TIMEDATA=$(date +"%Y%m%d")
 
 #检测当前用户是否是root用户
 if [[ "$(whoami)" != "root" ]]; then
@@ -31,8 +32,8 @@ cd $Adir
 tar zxvf $Opensslv.tar.gz
 cd $Adir/$Opensslv
 ./config --prefix=/usr/local/openssl -fPIC no-gost && make -j$UJ depend && make install
-\mv /usr/bin/openssl /usr/bin/openssl.old
-\mv /usr/include/openssl /usr/include/openssl.old
+\mv /usr/bin/openssl /usr/bin/openssl.$TIMEDATA
+\mv /usr/include/openssl /usr/include/openssl.$TIMEDATA
 ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
 ln -s /usr/local/openssl/include/openssl /usr/include/openssl
 echo "/usr/local/openssl/lib" >> /etc/ld.so.conf
@@ -42,15 +43,16 @@ openssl version -a | awk 'NR==1' > $Adir/openssl_up.log
 
 
 #updata ssh
-\mv /etc/ssh /etc/ssh.old
+\mv /etc/ssh /etc/ssh.$TIMEDATA
 mkdir -p /etc/ssh
 cd $Adir
 tar -zxvf $Opensshv.tar.gz
 cd $Adir/$Opensshv
-./configure --prefix=/usr --sysconfdir=/etc/ssh  --with-zlib=/usr/local/zlib --with-ssl-dir=/usr/local/openssl  --with-md5-passwords --disable-etc-default-login --with-ssl-engine --mandir=/usr/share/man && make -j$UJ && make install
+#./configure --prefix=/usr --sysconfdir=/etc/ssh  --with-zlib=/usr/local/zlib --with-ssl-dir=/usr/local/openssl/include/openssl  --with-md5-passwords --disable-etc-default-login --with-ssl-engine --mandir=/usr/share/man && make -j$UJ && make install
+./configure --prefix=/usr --sysconfdir=/etc/ssh --with-md5-passwords --with-ssl-dir=/usr/local/openssl --without-hardening && make -j$UJ && make install
 
 
-
+\mv /etc/init.d/sshd /etc/init.d/sshd.$TIMEDATA
 cp -p contrib/redhat/sshd.init /etc/init.d/sshd
 chmod +x /etc/init.d/sshd
 sed -i 's@/sbin/restorecon /etc/ssh/ssh_host_key.pub@#/sbin/restorecon /etc/ssh/ssh_host_key.pub@' /etc/init.d/sshd 
